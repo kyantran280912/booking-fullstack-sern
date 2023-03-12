2912c4e931e0c60f61c9e34ee05851e567af0238
Violet Hair <91548050+vanbinh280912@users.noreply.github.com>
2023-03-12 23:25:12 +0700

    ket noi db voi sequelize

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const viewEngine = require("./config/viewEngine");
 const initWebRoutes = require("./routes/web");
 require("dotenv").config();
+import connectDB from "./config/connectDB";
 
 let app = express();
 
@@ -12,6 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 
 viewEngine(app);
 initWebRoutes(app);
+
+connectDB();
 
 let port = process.env.PORT || 6969;
 app.listen(port, () => {
